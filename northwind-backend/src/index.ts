2e4b0b89ba1e76256f114141ebe99e76eb5cd789
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2026-05-25 22:16:19 +0600

    Add Sentry integration for error tracking and user context middleware

--- northwind-backend/src/index.ts
+++ northwind-backend/src/index.ts
@@ -5,6 +5,7 @@ import "dotenv/config";
 // Deployment Related
 import fs from "node:fs";
 import path from "node:path";
+import * as Sentry from "@sentry/node";
 
 import { clerkMiddleware } from '@clerk/express'
 import { clerkWebhookHandler } from "./webhooks/clerk";
@@ -16,6 +17,7 @@ import productRouter from "./routes/productRouter";
 import streamRouter from "./routes/streamRouter";
 import checkoutRouter from "./routes/checkoutRouter";
 import { polarWebhookHandler } from "./webhooks/polar";
+import { sentryClerkUserMiddleware } from "./middleware/sentryClerkUser";
 
 const env = getEnv();
 const app = express();
@@ -34,6 +36,7 @@ app.post("/webhooks/polar", rawJson, (req: Request, res: Response) => {
 app.use(express.json());
 app.use(cors());
 app.use(clerkMiddleware());
+app.use(sentryClerkUserMiddleware);
 
 app.use("/health", (_req, res) => {
   res.json({ ok: true })
@@ -64,7 +67,18 @@ if (fs.existsSync(publicDir)) {
   });
 }
 
-// TODO: add error handler middleware
+
+Sentry.setupExpressErrorHandler(app);
+app.use(
+  (_err: unknown, _req: express.Request, res: express.Response, _next: express.NextFunction) => {
+    const sentryId = (res as express.Response & { sentry?: string }).sentry;
+
+    res.status(500).json({
+      error: "Internal server error",
+      ...(sentryId !== undefined && { sentryId }),
+    });
+  },
+);
 
 app.listen(env.PORT, () => {
   console.log(`Listening to PORT ${env.PORT}`)
