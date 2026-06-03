b9d9bf28cf5c9f8f6028b6fc855d436a214f5536
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2026-06-03 21:12:48 +0600

    feat: add product catalog and homepage components with optimized image handling

--- northwind-frontend/src/Router.jsx
+++ northwind-frontend/src/Router.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router";
 import App from "./App";
+import HomePage from "./pages/HomePage";
 
 export const router = createBrowserRouter([
     {
@@ -8,7 +9,7 @@ export const router = createBrowserRouter([
         children: [
             {
                 index: true,
-                element: <div>Hello World</div>,
+                element: <HomePage />,
             },
         ],
     },
