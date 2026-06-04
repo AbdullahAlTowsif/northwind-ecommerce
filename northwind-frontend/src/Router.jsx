adc6f133f1b8a7c1ff5ba24a329af879ed1dc882
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2026-06-04 21:56:36 +0600

    feat: implement cart page with empty state and loading skeletons

--- northwind-frontend/src/Router.jsx
+++ northwind-frontend/src/Router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router";
 import App from "./App";
 import HomePage from "./pages/HomePage";
+import CartPage from "./pages/CartPage";
 
 export const router = createBrowserRouter([
     {
@@ -13,4 +14,8 @@ export const router = createBrowserRouter([
             },
         ],
     },
+    {
+        path: "/cart",
+        element: <CartPage />,
+    }
 ]);
